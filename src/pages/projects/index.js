437b214805fbadd629adb7e0365e543384e762ff
Muthukumaran Navaneethakrishnan <muthuishere@gmail.com>
2023-12-20 13:39:18 +0530

    updated for chat

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,12 +1,48 @@
-import { useEffect, useState } from "react";
-import { axiosAiClient } from "@/axiosClient";
+import {useEffect, useState} from "react";
+import {axiosAiClient} from "@/axiosClient";
 import MainLayout from "@/components/layout/MainLayout";
 import withLoggedIn from "@/guards/withLoggedIn";
 import Link from "next/link";
 
 export const getServerSideProps = withLoggedIn(async (context) => {
-    return { props: {} };
+    return {props: {}};
 });
+
+export function DisplayProject({index, project}) {
+
+    return <div
+        key={index}
+        className="break-inside-avoid p-4 bg-white rounded-lg shadow-md mb-4"
+    >
+
+        <h2 className="text-lg font-bold mb-2">{project.title}</h2>
+        <p className="text-gray-700 text-sm mb-4">
+            {project.description.length > 150
+                ? project.description.substring(0, 150) + "..."
+                : project.description}
+        </p>
+        <div className="text-gray-600 text-xs mb-4">
+            Date Created: {new Date(project.date_created).toLocaleDateString()}
+        </div>
+        <div className="mb-4">
+            <strong className="font-bold">Categories:</strong>
+            <ul className="list-disc list-inside">
+                {project.categories.map((category, catIndex) => (
+                    <li key={catIndex} className="text-gray-600 text-sm">
+                        {category}
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <Link
+            href={`/projects/${project.id}`}
+            className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors"
+        >
+            View
+        </Link>
+    </div>
+}
 
 export default function AllProjects() {
     const [projects, setProjects] = useState([]);
@@ -17,16 +53,19 @@ export default function AllProjects() {
         category: "",
     });
 
+    const isFilterSelected = () => {
+        return filter.theme !== "" || filter.domain !== "" || filter.category !== "";
+    };
     useEffect(() => {
         axiosAiClient.get("/api/allprojects").then((response) => {
             setProjects(response.data);
-            setFilteredProjects(response.data);
+            setFilteredProjects([]);
         });
     }, []);
 
     const handleFilterChange = (event) => {
-        const { name, value } = event.target;
-        setFilter({ ...filter, [name]: value });
+        const {name, value} = event.target;
+        setFilter({...filter, [name]: value});
     };
 
     useEffect(() => {
@@ -57,20 +96,20 @@ export default function AllProjects() {
 
                 {/* Filter Bar */}
                 <div className="flex space-x-4 mb-4">
-                <select name="theme" onChange={handleFilterChange} value={filter.theme} className="form-select">
-  <option value="">Select Theme</option>
-  {
-    Array.from(
-      new Set(
-        projects.map(project => 
-          project.theme.charAt(0).toUpperCase() + project.theme.slice(1).toLowerCase()
-        )
-      )
-    ).map((theme, index) => (
-      <option key={index} value={theme}>{theme}</option>
-    ))
-  }
-</select>
+                    <select name="theme" onChange={handleFilterChange} value={filter.theme} className="form-select">
+                        <option value="">Select Theme</option>
+                        {
+                            Array.from(
+                                new Set(
+                                    projects.map(project =>
+                                        project.theme.charAt(0).toUpperCase() + project.theme.slice(1).toLowerCase()
+                                    )
+                                )
+                            ).map((theme, index) => (
+                                <option key={index} value={theme}>{theme}</option>
+                            ))
+                        }
+                    </select>
 
                     <select
                         name="domain"
@@ -111,44 +150,23 @@ export default function AllProjects() {
                     </select>
                 </div>
 
-                <div className="columns-1 md:columns-2 lg:columns-3 gap-4">
-                    {filteredProjects.map((project, index) => (
-                        <div
-                            key={index}
-                            className="break-inside-avoid p-4 bg-white rounded-lg shadow-md mb-4"
-                        >
-                 {/* Project details as before */}
-<h2 className="text-lg font-bold mb-2">{project.title}</h2>
-<p className="text-gray-700 text-sm mb-4">
-    {project.description.length > 150
-        ? project.description.substring(0, 150) + "..."
-        : project.description}
-</p>
-<div className="text-gray-600 text-xs mb-4">
-    Date Created: {new Date(project.date_created).toLocaleDateString()}
-</div>
-<div className="mb-4">
-    <strong className="font-bold">Categories:</strong>
-    <ul className="list-disc list-inside">
-        {project.categories.map((category, catIndex) => (
-            <li key={catIndex} className="text-gray-600 text-sm">
-                {category}
-            </li>
-        ))}
-    </ul>
-</div>
 
-<Link
-    href={`/projects/${project.id}`}
-    className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors"
->
-    View
-</Link>
-</div>
-))}
-</div>
-</div>
-</MainLayout>
-);
+
+                    {!isFilterSelected() ? (
+                        <div className="columns-1 md:columns-2 lg:columns-3 gap-4 mt-3">
+                        <div className="error-message">Please select a filter criteria.</div>
+                        </div>
+                    ) : (
+                        <div className="columns-1 md:columns-2 lg:columns-3 gap-4">
+                            {filteredProjects.map((project, index) => (
+                                <DisplayProject key={index} project={project} />
+                            ))}
+                        </div>
+                    )}
+
+
+            </div>
+        </MainLayout>
+    );
 }
            
